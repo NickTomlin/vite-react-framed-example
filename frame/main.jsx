34ca91fffb20869d64d7bbc506664e9e294449ac
Nick Tomlin <NickTomlin@users.noreply.github.com>
2023-05-02 07:04:43 -0500

    chore: messing around with srcDoc

--- frame/main.jsx
+++ frame/main.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ReactDOM from 'react-dom/client'
 
 // what we wnat to do is have the frame loaded in a srcDoc but still be able to talk to the bundle...
 function Wrapper () {
-    return <div>HI from frame!!</div>
+    useEffect(() => {
+        console.log('send')
+        window.postMessage('hello', '*')
+    }, [])
+    // listen for postMessage from host
+    useEffect(() => {
+        function hi (e) {
+            console.log('message from host', e)
+        }
+        window.addEventListener('message', hi)
+        return () => {
+            window.removeEventListener('message', hi)
+        }
+    }, [])
+
+    useEffect(() => {
+        // fetch('https://example.com')
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               console.log(this.responseText.slice(0, 10));
+            }
+        }
+        xhttp.open("GET", "https://httpbin.org/get?foo=bar", true);
+        xhttp.send();
+
+    }, [])
+    return <div>
+        HI from frame!!
+        <a target={"_blank"} href={"http://www.google.com"}>Google</a>
+    </div>
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- host/HostApp.jsx
+++ host/HostApp.jsx
@@ -1,25 +1,15 @@
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
 function HostApp() {
-    const frameRef = useRef()
+    const frameRef = useRef();
+    useEffect(() => {
+        console.log(frameRef.current.contentWindow)
+        window.postMessage('hello', '*')
+    }, [])
     return (
         <>
             <h1>Host</h1>
-            <iframe ref={frameRef} style={{ height:"100vh", width: "100vw"}}  srcDoc={`
-                <!DOCTYPE html>
-                <html lang="en">
-                  <head>
-                    <meta charset="UTF-8" />
-                    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                    <title>Frame</title>
-                  </head>
-                  <body>
-                    <div id="root"></div>
-                    <script type="module" src="/frame/main.jsx"></script>
-                  </body>
-                </html>
-             `} />
+            <iframe sandbox={"allow-scripts"} ref={frameRef} style={{ height:"100vh", width: "100vw"}}  src={"/frame.html"} />
         </>
     )
 }
